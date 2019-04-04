32c45a4a2b5427d1713d546e224f2e4dc3c4ba81
hangjunhe <hangjunhe@ecarx.com.cn>
2019-04-04 17:41:52 +0800

    first commit

---
+++ XMLResolve.py
@@ -0,0 +1,63 @@
+#coding=utf-8
+
+#通过minidom解析xml文件
+import xml.dom.minidom as xmldom
+import os
+import sys
+
+class XMLResolve():
+
+    #获取 某类标签的所有属性值
+    @staticmethod
+    def getElementAttribute(xmlfilepath,elementName,elementAttribute):
+        # 得到文档对象
+        domobj = xmldom.parse(xmlfilepath)
+        # 得到元素对象
+        elementobj = domobj.documentElement
+        # 获得子标签
+        subElementObj = elementobj.getElementsByTagName(elementName)
+        # 获得标签属性值
+        for i in range(len(subElementObj)):
+            print (subElementObj[i].getAttribute(elementAttribute))
+
+    #根据标签名字获取标签
+    def getElementByName(self,xmlfilepath,tagElement,elementName):
+        # 得到文档对象
+        domobj = xmldom.parse(xmlfilepath)
+        # 得到元素对象
+        elementobj = domobj.documentElement
+        # 获得子标签
+        subElementObj = elementobj.getElementsByTagName(tagElement)
+        #print (len(subElementObj))
+        # 获得标签属性值
+        for i in range(len(subElementObj)):
+            print (subElementObj[i].getAttribute("name"))
+            print (subElementObj[i].getAttribute("fetch"))
+            print (subElementObj[i].getAttribute("review"))
+
+   # def predict(self, x):
+    #    check_ans = eval("Test." + Test.state2function[state])(x)  # 调用Test类中的方法
+
+if __name__ == '__main__':
+    xmlFile = sys.argv[1] #拿到xml 文件
+    if not os.path.exists(xmlFile):
+        print (xmlFile,"文件不存在")
+        exit(0)
+    xmlResolve = XMLResolve()#xmlFile
+    functionName=sys.argv[2] #要执行python 代码的那个方法名
+    argcount = 0
+    if not hasattr(xmlResolve,functionName):
+        print ("没有你所请求的%s方法属性"%(functionName))
+        exit(0)
+    else:
+        fun = getattr(xmlResolve,functionName)
+        argcount = fun.__code__.co_argcount
+
+    args = [xmlFile] + sys.argv[3:len(sys.argv)]
+    #print args
+    if len(args) != argcount:
+        print "参数个人不匹配"
+        exit(0)
+    kwargs = {}
+    eval("XMLResolve."+functionName)(*args, **kwargs) # 使用这个动态调用方法 、必须在方法头部 加上 @staticmethod
+
